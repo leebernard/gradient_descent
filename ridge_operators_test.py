83e6fa2158f691b3b329d2707617744fb5dcaa17
leebernard <ldbernard@asu.edu>
2023-09-02 14:14:47 -0700

    initial commit for ridge_operators_test.py, a script for testing ridge operations in skimage

--- ridge_operators_test.py
+++ ridge_operators_test.py
@@ -7,15 +7,15 @@ from skimage.filters import meijering, sato, frangi, hessian
 
 
 # create some test data
-im_path = 'image_data/slit_nofilter_screenshot.png'
+im_path = 'image_data/Image__2023-09-01__22-02-26_slit_background.tiff'
 from PIL import Image
 slit_im = Image.open(im_path)
 # convert to numpy array
-slit_data = np.asarray(slit_im.convert('L'))
+slit_data = np.asarray(slit_im)
 # crop the top
-slit_data = slit_data[150:, 500:1500]
+# slit_data = slit_data[150:, 500:1500]
 
-plt.imshow(slit_data)
+# plt.imshow(slit_data)
 # end create test data
 
 
@@ -23,20 +23,20 @@ def original(image, **kwargs):
     """Return the original image, ignoring any kwargs."""
     return image
 
-image = color.rgb2gray(slit_im)[150:, 500:1500]
+image = np.asarray(slit_im)
 cmap = plt.cm.gray
 
 plt.rcParams["axes.titlesize"] = "medium"
-axes = plt.figure(figsize=(10, 4)).subplots(2, 9)
+axes = plt.figure(figsize=(10, 4)).subplots(2, 3)
 for i, black_ridges in enumerate([True, False]):
     for j, (func, sigmas) in enumerate([
             (original, None),
-            (meijering, [1]),
-            (meijering, range(1, 5)),
-            (sato, [1]),
-            (sato, range(1, 5)),
-            (frangi, [1]),
-            (frangi, range(1, 5)),
+            # (meijering, [1]),
+            # (meijering, range(1, 5)),
+            # (sato, [1]),
+            # (sato, range(1, 5)),
+            # (frangi, [1]),
+            # (frangi, range(1, 5)),
             (hessian, [1]),
             (hessian, range(1, 5)),
     ]):
